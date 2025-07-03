5e0f6e39676ff79e616b05965099a3af0fa55606
preetam-90 <preetamkumar8873@gmail.com>
2025-07-03 07:31:09 -0400

    Fix hydration issues in site header

--- components/layouts/site-header.tsx
+++ components/layouts/site-header.tsx
@@ -15,67 +15,64 @@ export function SiteHeader() {
   const { isShowNavBackground } = useNavbarScrollOverlay()
   return (
     <header
-      className={cn(
-        'fixed inset-x-0 top-0 z-40 w-full transition duration-200',
-        {
-          'bg-background/80': isShowNavBackground,
-        }
-      )}
+      className="fixed inset-x-0 top-0 z-40 w-full h-16"
     >
-      <div className="container flex h-16 max-w-(--breakpoint-2xl) items-center space-x-4 sm:justify-between sm:space-x-0">
-        <MainNav items={siteConfig.mainNav} />
-        <MobileNav items={siteConfig.mainNav} />
-        <div className="flex flex-1 items-center justify-end space-x-4">
-          <div className="w-full flex-1 md:w-auto md:flex-none">
-            <CommandMenu />
+      <div className="container h-full">
+        <div className="flex h-full items-center justify-between">
+          <MainNav items={siteConfig.mainNav} />
+          <MobileNav items={siteConfig.mainNav} />
+          <div className="flex items-center space-x-4">
+            <div className="w-full flex-1 md:w-auto md:flex-none">
+              <CommandMenu />
+            </div>
+            <nav className="flex items-center space-x-1">
+              <Link
+                href={siteConfig.links.github}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <div
+                  className={buttonVariants({
+                    size: 'icon',
+                    variant: 'ghost',
+                  })}
+                >
+                  <Icons.gitHub className="size-5" />
+                  <span className="sr-only">GitHub</span>
+                </div>
+              </Link>
+              <Link
+                href={siteConfig.links.twitter}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <div
+                  className={buttonVariants({
+                    size: 'icon',
+                    variant: 'ghost',
+                  })}
+                >
+                  <Icons.twitter className="size-5 fill-current" />
+                  <span className="sr-only">Twitter</span>
+                </div>
+              </Link>
+              <Link
+                href={siteConfig.links.buyMeACoffee}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <div
+                  className={buttonVariants({
+                    size: 'icon',
+                    variant: 'ghost',
+                  })}
+                >
+                  <Icons.buyMeACoffee className="size-5" />
+                  <span className="sr-only">Buy me a coffee</span>
+                </div>
+              </Link>
+            </nav>
           </div>
-          <nav className="flex items-center space-x-1">
-            <Link
-              href={siteConfig.links.github}
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div
-                className={buttonVariants({
-                  size: 'icon',
-                  variant: 'ghost',
-                })}
-              >
-                <Icons.gitHub className="size-5" />
-                <span className="sr-only">GitHub</span>
-              </div>
-            </Link>
-            <Link
-              href={siteConfig.links.twitter}
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div
-                className={buttonVariants({
-                  size: 'icon',
-                  variant: 'ghost',
-                })}
-              >
-                <Icons.twitter className="size-5 fill-current" />
-                <span className="sr-only">Twitter</span>
-              </div>
-            </Link>
-            <Link
-              href={siteConfig.links.buyMeACoffee}
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div
-                className={buttonVariants({
-                  size: 'icon',
-                  variant: 'ghost',
-                })}
-              >
-                <Icons.buyMeACoffee className="size-5" />
-                <span className="sr-only">Buy me a coffee</span>
-              </div>
-            </Link>
-          </nav>
         </div>
       </div>
     </header>
